f38b34617661bb52b7d8eb329753a223dfff28aa
Naifish Ali <nf533051@dal.ca>
2018-05-11 08:14:06 -0300

    temporary user interface for the app

--- Resume Filter/Resume Filter/Form1.cs
+++ Resume Filter/Resume Filter/Form1.cs
@@ -16,5 +16,10 @@ namespace Resume_Filter
         {
             InitializeComponent();
         }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
